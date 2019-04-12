4000cf229e8cacb0eb2683f6f90a7f6bded1bd5a
solomonmuheki <solomonwit@gmail.com>
2019-04-12 13:45:47 +0300

    javascript code calculating interest

--- Firefox/calculate.js
+++ Firefox/calculate.js
@@ -1,3 +1,22 @@
-function calculate(){
 
-}
+		function calculate(){
+			
+		AmountOfTheLoan= parseInt(document.form1.amount_of_loan.value);
+		AnnualInterest=parseInt(document.form1.annual_interest.value);
+		RepaymentPeriod=parseInt(document.form1.Repayment_period.value);
+		
+		
+		
+		
+		var TotalInterest=((AnnualInterest/100)*AmountOfTheLoan)*RepaymentPeriod;
+		
+		TotalPayment=AmountOfTheLoan+TotalInterest;
+
+		MonthlyPayment=TotalPayment/(RepaymentPeriod*12);
+		
+		document.getElementById("lblTpayment").innerHTML=TotalPayment;
+		document.getElementById("lblTinterest").innerHTML=TotalInterest;
+		document.getElementById("lblmonthly").innerHTML=MonthlyPayment;
+alert("hey"+TotalPayment);
+	}
+	

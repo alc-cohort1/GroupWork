c317ea2a33eefe09e353487acf16becb41f44ea9
jpetero <jpetexi@gmail.com>
2019-04-15 11:13:37 +0300

    Group FireFox

--- Group_FireFox/js/main.js
+++ Group_FireFox/js/main.js
@@ -6,7 +6,7 @@ function validate(){
     var annualInterest = parseFloat(document.loanForm.annualInterest.value);
     var repaymentPeriod = parseFloat(document.loanForm.repaymentPeriod.value);
     var zipCode = document.loanForm.zipCode.value;
-    var totalInterest = (annualInterest * loanAmount * repaymentPeriod / 100);
+    var totalInterest = ((annualInterest / 100 ) * loanAmount * repaymentPeriod);
     var totalPayment = (loanAmount + totalInterest);
     var monthlyPayment = (totalPayment / (repaymentPeriod * 12));
 

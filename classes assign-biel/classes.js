a34725db21dd131652c7614b978d8f516894b0aa
biel-chah <48758079+biel-chah@users.noreply.github.com>
2019-04-16 14:07:45 +0300

    classes

---
+++ classes assign-biel/classes.js
@@ -0,0 +1,27 @@
+class Rectangle {
+    constructor(height, width) {
+      this.height = height;
+      this.width = width;
+    }
+  }
+
+  class Point {
+    constructor(x, y) {
+      this.x = x;
+      this.y = y;
+    }
+
+    class Animal { 
+        speak() {
+          return this;
+        }
+        static eat() {
+          return this;
+        }
+      }
+      lass Rectangle {
+        constructor(height, width) {
+          this.height = height;
+          this.width = width;
+        }
+      }

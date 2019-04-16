7cd823a4469bb319e1f4f8aeec443a88a5c4b4da
tendomart <tmkmart@gmail.com>
2019-04-16 09:03:47 +0300

    Added Comments to html elements ,Javascript componets and refactored css

---
+++ Tendo/myJsObjects/person.js
@@ -0,0 +1,9 @@
+//This class is blueprint for all any type of person
+class{
+    constructor(name,age,gender,height){
+        this.name=name;
+        this.age=age;
+        this.gender=gender;
+        this.height=height;
+    }
+}

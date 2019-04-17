0dad50f38bc819e7d61c7599579047250d67f79e
BenjaminKyamanywa <benjaminkyam@gmail.com>
2019-04-17 11:37:36 +0300

    Removed Norad Class Project Folder.

---
+++ Benjamin_Kyamanywa/classes.js
@@ -0,0 +1,52 @@
+// Different class categories
+
+// Below is a class for motorbike
+var motorbike = new object ();
+motorbike.brand = "Harley Davidson Street 750";
+motorbike.type = "cruiser";
+motrobike.engineType = "Liquid Cooled";
+motorbike.condition = "New";
+motorbike.speedometer = "Digital";
+motorbike.tractionControl = "No";
+motorbike.odometer = "Digital";
+motorbike.antilockBreakingSystem = "Dual Channel";
+
+// Below is a class for footballTeam
+var footballTeam = new object ();
+footballTeam.club = "Leicester City F.C";
+footballTeam.league = "Premier League";
+footballTeam.city = "Leicester";
+footballTeam.manager = "Brendan Rogers";
+footballTeam.owner = "King Power"
+footballTeam.players = ["Kasper Schmeichel", "Danny Simpson", "Ben Chilwell", "Caglar Soyuncu", "Wes Morgan", "Jonny Evans", "Ricardo Pereira", "James Maddison", "Damarai Gray", "Jamy Vardy", "Shinji Okazaki", ];
+
+// Below is a class for child
+var child = new object ();
+child.name = "Daenerys Targaryen";
+child.age = 8;
+child.gender = "Female";
+child.interests = ["Singing", "Swimming", "Travelling", "Gamming", "Dancing", "Reading",];
+
+// Below is a class for novel
+var novel = new object ();
+novel.author = "Nicholas Sparks";
+novel.country = "United States";
+novel.series = "The Notebook and The Wedding";
+novel.genre = "	Romance";
+novel.publisher = "Warner Books";
+novel.publicationDate = "October 1, 1996";
+novel.pages = 214;
+
+
+// Below is a class for laptop
+var laptop = new object ();
+laptop.brand = "Macbook Pro";
+laptop.color = "Space Gray";
+laptop.display = "Retina Display";
+laptop.processor = "2.6GHz 6-core Intel Core i7";
+laptop.storage = "256GB SSD";
+laptop.memory = "16GB of 2400MHz DDR4 onboard memor";
+laptop.graphics = "2.6GHz Radeon Pro 560X with 4GB of GDDR5 memory and automatic graphics switching";
+laptop.operatingSystem = "macOS";
+
+

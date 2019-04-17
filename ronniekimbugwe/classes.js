4c7df81bc60cf28ff7588f6056970628aefa3d6d
kimbugweronnie <kimbugweronnie@gmail.com>
2019-04-17 10:38:26 +0300

    edited comments

--- ronniekimbugwe/classes.js
+++ ronniekimbugwe/classes.js
@@ -3,7 +3,7 @@ class Cats {
     this.name = name;
     this.breed = breed;
     this.origin = origin;
-    //constructor with parameters name,breed and origin
+    //parameters name,breed and origin
   }
 
   description() {
@@ -13,7 +13,7 @@ class Cats {
 
 let Cats = new Object();
 {
-  // new object or class Cats called cat
+  // new object of class Cats called cat
   cat.name = "cat";
   cat["breed"] = "purina";
   cat["origin"] = "russia";
@@ -54,15 +54,15 @@ class Clans {
 
   description() {
     alert(
-      this.name + " totem is " + this.totem + "and the motto " + this.motto
+      this.name + " totem is " + this.totem + "and the motto is" + this.motto
     );
   }
 }
 
 let Clan = new Object();
 {
-  // new object or class Tiles called tile
-  clan.name = "Balangira";
+  // new object of class Clans called clan
+  clan.name = "Akasimba";
   clan["totem"] = "Engo";
   clan["motto"] = "Kiiso bwe kikulaba obulungi naawe okiraba.!";
   clan.description(); //function call
@@ -83,7 +83,7 @@ class Clouds {
 
 let cloud = new Object();
 {
-  // new object or class Clouds called cloud
+  // new object of class Clouds called cloud
   cloud.name = " ‎Cumulusclouds";
   cloud[isManMade] = false;
   cloud.description(); //function call
@@ -112,7 +112,7 @@ class Shoes {
 
 let shoe = new Object();
 {
-  // new object or class Shoes called shoe
+  // new object of class Shoes called shoe
   shoe.name = " ‎plimsoms";
   shoe[isAllsizes] = true;
   shoe[isUnisex] = true;
@@ -140,7 +140,7 @@ let belt = new Object();
 {
   // new object of class Belt called belt
   belt.name = " ‎zara";
-  belt[color] = "black";
+  belt["color"] = "black";
   belt[isUnisex] = false;
 
   belt.description(); //function call

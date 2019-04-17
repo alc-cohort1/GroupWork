8c44531c0e2ebc7cadc6f3725c821af3d8129b93
EmmellaBeatrice <danahbeat@gmail.com>
2019-04-17 13:37:07 +0300

    Adding object parameters

--- Beatrice/class.js
+++ Beatrice/class.js
@@ -1,32 +1,45 @@
+//Create class Teacher and instatiate it with an object
 var Teacher = new object ();
-Teacher.name = 'Melanie';
-Teacher.gender = '';
-Teacher.id = '';
-Teacher.salary = '';
-Teacher.school = '';
+//Add parameters 
+Teacher.name = 'Susan';
+Teacher.gender = 'Female';
+Teacher.salary = '900,000';
+Teacher.school = 'British School of kampala';
 
-var Phones = new object ();
-Animal.name = '';
-Animal.brand = '';
-Animal.size = '';
+//Create class Phone and instatiate it with an object
+var Phone = new object ();
+//Add parameters 
+Phone.name = 'iPhone X';
+Phone.brand = 'Apple';
+Phone.storage = '64GB';
+//Create method about with an alert message
+Phone.about = function(){
+    alert('i want an' + this.name + 'with' + this.storage);
+};
 
+//Create class Animal and instatiate it with an object
 var Animal = new object ();
-Animal.name = '';
-Animal.gender = '';
-Animal.age = '';
-Animal.habitant = '';
+//Add parameters 
+Animal.name = 'Blue whale';
+Animal.type = 'mammal';
+Animal.diet = 'Carnivore';
+Animal.size = '82 to 105 feet';
+Animal.habitat = 'oceans';
 
+//Create class Doctor and instatiate it with an object
 var Doctor = new object ();
-Doctor.name = '';
-Doctor.gender = '';
-Doctor.id = '';
-Doctor.salary = '';
-Doctor.hospital = '';
+//Add parameters 
+Doctor.name = 'Melanie';
+Doctor.gender = 'Female';
+Doctor.salary = '3,000,000';
+Doctor.hospital = 'Mulago';
 
+//Create class lake and instatiate it with an object
 var lake = new object ();
-lake.name = '';
-lake.location = '';
-lake.size = '';
+//Add parameters 
+lake.name = 'Bunyonyi';
+lake.location = 'south-western Ugand';
+lake.size = '7 kilometres';
 
 
 

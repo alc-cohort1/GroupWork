ed5273f1b4365794d301a7f4f4ba16b92320bb9d
katungye johnson <katungyejohnson@gmail.com>
2019-04-18 11:53:49 +0300

    added some comments

--- katungyeJohnson/classes.js
+++ katungyeJohnson/classes.js
@@ -1,8 +1,11 @@
+
+/* this is a class using the constractor method*/
 function vehicle (make, spead, color) {
     this.make = make;
     this.spead = spead;
     this.color = color;
     this.getName = function(){
+        //code that is executed when the class is called
         return this.make + "" + this.model;
     }
 }

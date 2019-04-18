c0f607cecc766c490dfe0721ecc8ac9d52a55e6d
kimbugweronnie <kimbugweronnie@gmail.com>
2019-04-18 15:55:29 +0300

    adding more objects to classes

--- ronniekimbugwe/classes.js
+++ ronniekimbugwe/classes.js
@@ -6,8 +6,14 @@ class Cats {
     //attributes name,breed and origin
   }
 
-  description() {
+  PlaceOforigin() {
     alert(this.name + "of  " + this.breed + "is from " + this.origin);
+  }
+  sound() {
+    alert(this.name + "meows and its from  " + this.origin);
+  }
+  size() {
+    alert(this.name + "bigger than other cats of  breed " + this.breed);
   }
 }
 
@@ -17,7 +23,23 @@ let cat = new Object();
   cat.name = "Mycat";
   cat["breed"] = "purina";
   cat["origin"] = "russia";
-  cat.description(); // function call
+  cat.PlaceOforigin(); // function call
+}
+let cat1 = new Object();
+{
+  // new object of class Cats called cat
+  cat.name = "Mycat1";
+  cat["breed"] = "Russian Blue";
+  cat["origin"] = "Russia";
+  cat.sound(); // function call
+}
+let cat2 = new Object();
+{
+  // new object of class Cats called cat
+  cat.name = "Mycat2";
+  cat["breed"] = "ragdoll";
+  cat["origin"] = "america";
+  cat.size(); // function call
 }
 class Tiles {
   //class called Tiles
@@ -33,6 +55,19 @@ class Tiles {
       this.name + "is of make " + this.make + "and is put " + this.location
     );
   }
+  placement() {
+    alert(
+      this.name +
+        "tiles and other " +
+        this.make +
+        "are placed in " +
+        this.location
+    );
+  }
+  price() {
+    alert(this.name + "tiles are  the most expensive tiles ");
+    // + this.make + "are placed in " + this.location);
+  }
 }
 
 let tile = new Object();
@@ -42,6 +77,22 @@ let tile = new Object();
   tile["make"] = "granite";
   tile["origin"] = "floor";
   tile.description(); //function call
+}
+let tile1 = new Object();
+{
+  // new object or class Tiles called tile
+  tile.name = "terrazo";
+  tile["make"] = "granite";
+  tile["origin"] = "floor";
+  tile.price(); //function call
+}
+let tile2 = new Object();
+{
+  // new object or class Tiles called tile
+  tile.name = "marble";
+  tile["make"] = "granite";
+  tile["origin"] = "floor";
+  tile.placement(); //function call
 }
 class Clans {
   //class called Clans
@@ -57,6 +108,14 @@ class Clans {
       this.name + " totem is " + this.totem + "and the motto is" + this.motto
     );
   }
+  size() {
+    alert(this.name + " is the biggest clan");
+  }
+  Mydescription() {
+    alert(
+      this.name + " my clan i dont eat or associate myself with" + this.totem
+    );
+  }
 }
 
 let clan = new Object();
@@ -64,24 +123,61 @@ let clan = new Object();
   // new object of class Clans called clan
   clan.name = "Akasimba";
   clan["totem"] = "Engo";
-  clan["motto"] = "Kiiso bwe kikulaba obulungi naawe okiraba.!";
+  clan["motto"] = "Kiiso bwe kikulaba obulungi naawe okiraba.!"; //
   clan.description(); //function call
+}
+let clan1 = new Object();
+{
+  // new object of class Clans called clan
+  clan1.name = "Ngabbi";
+  clan1["totem"] = "jjerengesa";
+  clan1["motto"] = "Kiiso bwe kikulaba obulungi naawe okiraba.!";
+  clan1.size(); //function call
+}
+let clan2 = new Object();
+{
+  // new object of class Clans called clan
+  clan2.name = "Mutima";
+  clan2["totem"] = "Mawuggwe (Lungs)";
+  clan2["motto"] = "Kifa ennyanja, omuvubi y'abika."; //
+  clan2.description(); //function call
 }
 class Clouds {
   //class called Clouds
-  clouds(name, isManMade) {
+  clouds(name, isManMade, isRare) {
     this.name = name;
     this.isManMade = isManMade;
+    this.isRare = isRare;
 
     //constructor with attributes name,isManMade
   }
 
   description() {
-    alert(this.name + "is man made " + this.isManMade);
+    alert(this.name + "is man made :" + this.isManMade);
+  }
+  created() {
+    alert(this.name + "is man made ");
+  }
+  seen() {
+    alert("is " + this.name + "rare?:" + this.isRare);
   }
 }
 
 let cloud = new Object();
+{
+  // new object of class Clouds called cloud
+  cloud.name = " ‎Cumulusclouds";
+  cloud["isManMade"] = false;
+  cloud.description(); //function call
+}
+let cloud1 = new Object();
+{
+  // new object of class Clouds called cloud
+  cloud.name = " ‎Cumulusclouds";
+  cloud["isManMade"] = false;
+  cloud.description(); //function call
+}
+let cloud2 = new Object();
 {
   // new object of class Clouds called cloud
   cloud.name = " ‎Cumulusclouds";
@@ -108,6 +204,12 @@ class Shoes {
         this.isAllsizes
     );
   }
+  sales() {
+    alert(this.name + "have high sales");
+  }
+  comparision() {
+    alert(this.name + "are of all sizes: " + this.isAllsizes);
+  }
 }
 
 let shoe = new Object();
@@ -119,6 +221,25 @@ let shoe = new Object();
 
   shoe.description(); //function call
 }
+let shoe1 = new Object();
+{
+  // new object of class Shoes called shoe
+  shoe.name = " ‎plimsoms";
+  shoe["isAllsizes"] = true;
+  shoe["isUnisex"] = true;
+
+  shoe.description(); //function call
+}
+let shoe2 = new Object();
+{
+  // new object of class Shoes called shoe
+  shoe.name = " ‎plimsoms";
+  shoe["isAllsizes"] = true;
+  shoe["isUnisex"] = true;
+
+  shoe.description(); //function call
+}
+
 class Belts {
   //class called Belts
   belts(name, isUnisex, color) {

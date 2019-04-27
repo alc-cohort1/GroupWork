f879bdd2d687dd40597f8aa276a448a3de1196cc
omega judith <omegajudith@gmail.com>
2019-04-27 18:39:32 +0300

    node port

--- firstNodeJs/index.js
+++ firstNodeJs/index.js
@@ -1,10 +1,14 @@
+
+/*
 var http = require('http');
 
 var server = http.createServer(function(req, res){
-    console.log('request was made' + req.url);
+    console.log('request was made' + req.url);                             // <== This was to see if my port is running
     res.writeHead(200, {'content-type':'text/plain'});
     res.end('hello members of ALC class!');
-})
+});
 
 server.listen(3000,'127.0.0.1');
-console.log('Time to get hands on with nodejs')
+console.log('Time to get hands on with nodejs');
+*/
+

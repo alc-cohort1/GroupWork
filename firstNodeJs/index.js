865c1b4098e3917d2eb2744eb9a200db74d0f786
omega judith <omegajudith@gmail.com>
2019-04-27 16:04:28 +0300

    routing injs

--- firstNodeJs/index.js
+++ firstNodeJs/index.js
@@ -1,30 +1,10 @@
+var http = require('http');
 
+var server = http.createServer(function(req, res){
+    console.log('request was made' + req.url);
+    res.writeHead(200, {'content-type':'text/plain'});
+    res.end('hello members of ALC class!');
+})
 
-const http = require('http'),
-      url = require('url'),
- 
-makeServer = function (request,response){
-   let path = url.parse(request.url).pathname;
-   console.log(path);
-   if(path === '/'){
-      response.writeHead(200,{'Content-Type':'text/plain'});
-      response.write('Hello world');
-   }
-   else if(path === '/about'){
-     response.writeHead(200,{'Content-Type':'text/plain'});
-     response.write('About page');
-   }
-   else if(path === '/blog'){
-     response.writeHead(200,{'Content-Type':'text/plain'});
-     response.write('Blog page');
-   }
-   else{
-     response.writeHead(404,{'Content-Type':'text/plain'});
-     response.write('Error page');
-   }
-   response.end();
- },
-server = http.createServer(makeServer);
-server.listen(3000,()=>{
- console.log('Node server created at port 3000');
-});
+server.listen(3000,'127.0.0.1');
+console.log('Time to get hands on with nodejs')

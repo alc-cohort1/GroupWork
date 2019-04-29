f61b1febde7efac6b10028d1e5047a13ba2bf91e
katungye johnson <katungyejohnson@gmail.com>
2019-04-29 10:19:41 +0300

    profile page

--- katungyeJohnson/classes.js
+++ katungyeJohnson/classes.js
@@ -1,21 +1,26 @@
 
+"strict";
 /* this is a class using the constractor method*/
 function vehicle (make, spead, color) {
     this.make = make;
     this.spead = spead;
     this.color = color;
+
     this.getName = function(){
         //code that is executed when the class is called
         return this.make + "" + this.model;
     }
 }
 // this class is called when this.getInfo points to the fuction
-function Apple (type) {
+function Apple (type, weight) {
     this.type = type;
-    this.color = "red";
+    this.weight = weight;
+    
     this.getInfo = function() {
-        return this.color + ' ' + this.type + ' apple';
+        return this.color + ' ' + this.type;
     };
 }
+
 //the code below is a besic way to diclare an object 
 var student = {firstName:"Johnson", lastName:"katungye", age:28, course:"software development"};
+ console.log(student);

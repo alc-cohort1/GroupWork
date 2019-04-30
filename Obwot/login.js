1991cbcea4af6c6eaddb96cfad684633e2261843
Solomon Obwot <solobwot@gmail.com>
2019-04-30 16:04:52 +0300

    Posting data to the database from HTML

--- Obwot/login.js
+++ Obwot/login.js
@@ -4,6 +4,8 @@ const express = require('express')
 const app= express()
 //calling the body-parse module/packages
 const parser = require('body-parser')
+//calling the mysql module
+const mysql = require('mysql')
 
 
 app.use(express.static('./templates'))
@@ -11,11 +13,33 @@ app.use(express.static('./templates'))
 app.use(parser.urlencoded({extended: false}))
 
 
+const getConnection = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	database: 'obwot',
+
+}) 
 
 //handling the post request from the HTML form
 app.post('/login', (req,res)=>{
 
-console.log("the name entered is"+ req.body.name)
+console.log("the name entered is" + req.body.name)
+const name = req.body.name
+const location = req.body.location
+const password = req.body.password
+
+// const querystring = "INSERT INTO `obwot`.`login`(`name`,`location`,`password` VALUES(?,?,?))"
+const querystring = "INSERT INTO `login` (`name`, `location`, `password`) VALUES (?,?,?)";
+
+getConnection.query(querystring,[name,location,password],(err, results, field)=>{
+
+if (err) {
+	console.log('an error has occured' +err)
+	res.status(500)
+	return
+}
+
+})
 });
 
 // //getting the data from the form

2c0229061fc40ec8da0c87428cf1a24e3bc3013c
peter <mugonyipeter1@gmail.com>
2019-05-07 06:45:16 -0700

    updated cal

--- opera/calculator.js
+++ opera/calculator.js
@@ -1,21 +1,60 @@
 //intialisation of variable first to 0
-var borrowed_amount = 0;
-var interest = null;
-var interest_rate = interest / 100;
-var number_of_years = 0;
-var monthlypayment = 0;
-var totalpayment = 0;
-var totalinterest = 0;
+var loan_amount = getElementById.loan_data.loan_amount;
+var annual_interest = 5;
+
+var number_of_years = 5;
+var monthly_payment = 0;
+var total_payment = 0;
+var total_interest = 5;
 
 function calculate_interest() {
-  var borrowed_amount = document.myform.borrowed_amount.value;
-  var interest = document.myform.interest.value;
-  var interest_rate = document.myform.interest_rate.value;
-  var number_of_years = document.myform.number_of_years.value;
+  var loan_amount = document.getElementById("loan_amount").value;
+  var annual_interest = document.getElementById("annual_interest").value;
+  var number_of_years = document.getElementById("repayment_period").value;
+  alert(
+    "yello " + loan_amount + "  " + annual_interest + "  " + number_of_years
+  );
+  var interest_rate = annual_interest / 100;
+  monthly_payment = (loan_amount * interest_rate) / 12;
+  total_interest = monthly_payment * 12 * number_of_years;
+  total_payment = total_interest + loan_amount;
 
-  monthlypayment = (borrowed_amount * interest_rate) / 12;
-  totalpayment = borrowed_amount + monthlypayment * 60;
-  totalinterest = monthlypayment * 60;
+  // alert("hello !" + monthly_payment);
+  //alert("hello !" + monthly_payment);
+
+  document.getElementById("total_interest").innerHTML = total_interest;
+
+  document.getElementById("total_payment").innerHTML = total_payment;
+
+  alert(
+    "monthly payment is " +
+      monthly_payment +
+      " " +
+      "Total Interest is " +
+      total_interest +
+      " total payment is " +
+      total_payment
+  );
+
+  // monthly_payment = (loan_amount*interest_rate
+
+  /////////////////////////
+  //var loan_amount = document.loan_data.loan_amount.value;
+  // var annual_interest = document.loan_data.annual_interest.value;
+
+  //var repayment_period = document.loan_data.repayment_period.value;
+  //repayment_period;
+
+  //  monthly_payment = (loan_amount * interest_rate) / 12;
+
+  //alert("value   " + monthly_payment);
+  //total_payment = repayment_period + monthlypayment * 60;
+  //alert("value   " + total_payment);
+  //total_interest = monthlypayment * 60;
+  //monthly_payment = document.getElementById("monthly_payment").innerHTML;
+  //total_payment = document.getElementById("total_payment").innerHTML;
+  //total_interest = document.getElementById("total_interest").innerHTML;
+  //Event.preventDefault();
 }
 // monthly payment
 // total payment for 5 years

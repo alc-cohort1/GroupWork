33fddb3dfd027f9a69881f0d54dd5c54e781ba55
bongomin <bongomindaniel@gmail.com>
2019-05-17 17:25:11 +0300

    rebase

--- Bongomin-Daniel/app_toyota/templates/js/validation.js
+++ Bongomin-Daniel/app_toyota/templates/js/validation.js
@@ -1,22 +1,48 @@
 // validation function
 function validation() { // declaring fields
-    var customerId = document.getElementById('customerId').Value;
-    var name = document.getElementById('name').Value;
-    var state = document.getElementById('state').value;
-    var retailCustomer = document.getElementById('retailCustomer').value;
-    var partNumber = document.getElementById('partNumber').value;
-    var description = document.getElementById('description').value;
-    var price = document.getElementById('price').value;
-    var quantity = document.getElementById('quantity').value;
-    var cost = document.getElementById('cost').value;
-    var salesTax = document.getElementById('salesTax').value;
-    var salesAndHanding = document.getElementById('salesAndHanding').value;
-    var total = document.getElementById('total').value;
-    var compute_button = document.getElementById('computeBtn');
-    var new_Order_Btn = document.getElementById('newOrderBtn');
-    var exit_button = document.getElementById('exit');
 
-    // console.log(customerId)
+    var customerId = document.forms["toyotaForm"]["customerId"];
+    var name = document.forms["toyotaForm"]["name"];
+    var state = document.forms["toyotaForm"]["state"];
+    var retailCustomer = document.forms["toyotaForm"]["retailCustomer"];
+    var partNumber = document.forms["toyotaForm"]["partNumber"];
+    var description = document.forms["toyotaForm"]["description"];
+    var price = document.forms["toyotaForm"]["price"];
+    var quantity = document.forms["toyotaForm"]["quantity"];
+    var cost = document.forms["toyotaForm"]["cost"];
+    var salesTax = document.forms["toyotaForm"]["salesTax"];
+    var salesAndHanding = document.forms["toyotaForm"]["salesAndHanding"];
+    var total = document.forms["toyotaForm"]["total"];
+    var compute_button = document.forms["toyotaForm"]["compute_button"];
+    var new_Order_Btn = document.forms["toyotaForm"]["new_Order_Btn"];
+    var exit_button = document.forms["toyotaForm"]["exit_button"];
+
+
+    // computing the cost
+       if (price.value == "")
+           price.value = 0;
+       if (quantity.value == "")
+           quantity.value = 0;
+       var result = parseInt(quantity.value) * parseInt(price.value) 
+       if(!isNaN(result)) {
+           document.getElementById('cost').value = result;
+           cost.focus();
+           return false;
+       }else{
+           if(quantity.value=="" && price.value==""){
+               result ==0;
+           }
+       }
+
+    //    calculating the sales tax
+
+ 
+     
+
+       
+
+
+
 
 
 }

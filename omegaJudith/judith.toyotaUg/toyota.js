8a49092aee5e4b14e5d042271a3b1038a660e60b
omegajudith <omegajudith@gmail.com>
2019-05-17 09:49:28 +0300

    modifications

---
+++ omegaJudith/judith.toyotaUg/toyota.js
@@ -0,0 +1,31 @@
+function validData(){
+    //variables holding id values
+    var customer = Document.getElementById('customer').value
+    var name = Document.getElementById('name').value;
+    var state = Document.getElementById('state').value;
+    var retail = Document.getElementById('').value;
+    var part = Document.getElementById('').value;
+    var description = Document.getElementById('').value;
+    var price = Document.getElementById('').value;
+    var quantity = Document.getElementById('').value;
+    var over = Document.getElementById('').value;
+    var ups = Document.getElementById('').value;
+    var postal = Document.getElementById('').value;
+    var ground = Document.getElementById('').value;
+    var fedAir = Document.getElementById('').value;
+    var cost = Document.getElementById('').value;
+    var sales = Document.getElementById('').value;
+    var shipping = Document.getElementById('').value;
+    var total = Document.getElementById('').value;
+    //var compute = Document.getElementById('').value;
+    //var newOrder = Document.getElementById('').value;
+
+    if(customer == null || customer == ""){
+        alert('cant be blank');
+    }
+
+
+
+
+
+}
